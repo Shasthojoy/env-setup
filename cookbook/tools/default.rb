ba3ec9a85cf5645fde761c517758423fa14d5c13
yuu <yuu@users.noreply.github.com>
2017-04-25 21:54:38 +0900

    Add gnu-tar

--- cookbook/tools/default.rb
+++ cookbook/tools/default.rb
@@ -2,6 +2,7 @@ case node[:platform]
 when 'darwin'
     include_recipe 'mac.rb'
     package "grep --with-default-names"
+    package 'gnu-tar --with-default-names'
 when 'debian'
 when 'ubuntu'
     include_recipe 'debian.rb'
